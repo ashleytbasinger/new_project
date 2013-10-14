8023fba77ca58ed190b1cb37c3de994659211a34
Ashley Basinger <ashley.hipps@gmail.com>
2013-10-14 13:03:18 -0400

    ran migrations

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,36 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131012235228) do
+ActiveRecord::Schema.define(version: 20131014164558) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: true do |t|
+    t.string   "content"
+    t.integer  "photo_id"
+    t.integer  "timeline_id"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "photos", force: true do |t|
+    t.string   "title"
+    t.date     "date"
+    t.string   "image"
+    t.integer  "timeline_id"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "timelines", force: true do |t|
+    t.string   "title"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "username",                            null: false
