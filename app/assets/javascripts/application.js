036ed926e7aa887005272d180abe1c6d5a4860a0
Ashley Basinger <ashley.hipps@gmail.com>
2013-10-18 13:45:44 -0400

    dropped database, created demo timeline and added photos

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,14 @@
 //= require jquery
 //= require jquery_ujs
 //= require twitter/bootstrap
+//= require jquery.gridster
 //= require_tree .
+
+$(function(){ //DOM Ready
+ 
+    $(".gridster ul").gridster({
+        widget_margins: [10, 10],
+        widget_base_dimensions: [140, 140]
+    });
+ 
+});
